bceb677feda7fbc4a7e08451cf510757af62be83
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2024-07-13 18:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	//let index=-1;
+	s1=s1.toLowerCase();
+	let index=s1.indexOf(s2)
+	console.log(index);
 }
 
 // Please do not change the code below
